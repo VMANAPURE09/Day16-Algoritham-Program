3eecaf229113ba93b3e382b3ad8b3505b59cf31b
tutorial6 <manapure.vaibhav542@gmail.com>
2022-07-03 23:59:57 +0530

    Add Binary Tree Problem

---
+++ BinnarySearch/BinnarySearch/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace BinnarySearch
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            binaryTrees binaryTree = new binaryTrees();
+
+            binaryTree.Add(6);
+            binaryTree.Add(2);
+            binaryTree.Add(7);
+            binaryTree.Add(8);
+            binaryTree.Add(3);
+            binaryTree.Add(6);
+
+            binaryTrees.TravarsePreorder(binaryTree.Root);
+        }
+    }
+    }
+}
